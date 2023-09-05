27ff24e5e670fe815573c13ce12f12a8af4b1a69
Adeeb <AdeebtechnologyLab@gmail.com>
2023-09-05 23:39:16 +0500

    1st

---
+++ app/src/main/java/com/AdeebTechLab/GCT/BWP/College/SplashSecreen.java
@@ -0,0 +1,14 @@
+package com.AdeebTechLab.GCT.BWP.College;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashSecreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_secreen);
+    }
+}
