522b57196110c5a2fa5b0fccf544189676493cf9
Adeeb <AdeebtechnologyLab@gmail.com>
2023-09-06 15:12:07 +0500

    2nd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.AdeebTechLab.GCT.BWP.College"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/AdeebTechLab/GCT/BWP/College/SplashSecreen.java
+++ app/src/main/java/com/AdeebTechLab/GCT/BWP/College/SplashSecreen.java
@@ -2,6 +2,7 @@ package com.AdeebTechLab.GCT.BWP.College;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashSecreen extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class SplashSecreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_secreen);
+
+
+
+        Thread thread = new Thread(){
+
+            public void run(){
+                try {
+                    sleep( 4000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent = new Intent(  SplashSecreen.this , MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };thread.start();
     }
 }
